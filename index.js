934a8e880ac90b51f9e849f96053b3857edc89b6
Prospenda <24062023+Prospenda@users.noreply.github.com>
2023-09-29 11:14:19 +0100

    ajout de la méthode math.random

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // branch 1: Function MixedMessage
 // Mets la fonction en bas
-function MixedMessage (){
-    const tabMessages2 = ["Tous les enfants l'école", "Pour être riche, il faut travailler", "L'argent ne fait pas le bonheur"];
+    // const tabMessages2 = ["Tous les enfants l'école", "Pour être riche, il faut travailler", "L'argent ne fait pas le bonheur"];
     const tableauMessages = ["Bonjour", "Bienvenue", "Karibu", "Hello", "Vous y êtes presque"];
-}
-
+    
+const result = Math.floor(Math.random() * tableauMessages.length);
+console.log(tableauMessages[result]);
 // Mets le tableau des messages ici
 
 // Ajoutez la condition ici
 
 //Affichez les messages ici dans la console
-console.log()
