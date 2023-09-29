48f2972334ad07715af7c14d3f44c510cbde0533
Prospenda <24062023+Prospenda@users.noreply.github.com>
2023-09-29 11:51:22 +0100

    ajout de la méthode

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-// branch 1: Function MixedMessage
-// Mets la fonction en bas
     // const tabMessages2 = ["Tous les enfants l'école", "Pour être riche, il faut travailler", "L'argent ne fait pas le bonheur"];
     const tableauMessages = ["Bonjour", "Bienvenue", "Karibu", "Hello", "Vous y êtes presque"];
     
 const result = Math.floor(Math.random() * tableauMessages.length);
 console.log(tableauMessages[result]);
-// Mets le tableau des messages ici
-
-// Ajoutez la condition ici
-
-//Affichez les messages ici dans la console
