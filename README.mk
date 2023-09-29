d05b891329841bd31a5b308630f03f6cb59a0588
Ger-Cub <141303849+Ger-Cub@users.noreply.github.com>
2023-09-29 11:52:27 +0200

    Update README.mk
    Ajout du readme

---
+++ README.mk
@@ -0,0 +1,1 @@
+Ce projet consiste à créer un programme qui génère les messages de façon aléatoire. Pour ce faire, nous avons créer un tableau dans JavaScript qui contient les données (les messages). En suite nous avons créer une fontion qui permet de jénéer ou afficher un message aléatoir à chaque fois que l'utilisateur clique sur le bouton refresh.
