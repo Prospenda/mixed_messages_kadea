68be9b96260f7d3e9a5e9a4dcdb7f0263ce5ac67
Prospenda <24062023+Prospenda@users.noreply.github.com>
2023-09-29 10:13:09 +0100

    les variables

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // branch 1: Function MixedMessage
 // Mets la fonction en bas
 function MixedMessage (){
-    const tabMessages2 = ["Tous les enfants l'école", "Pour être riche, il faut travailler", "L'argent ne fait pas le bonheur"]
+    const tabMessages2 = ["Tous les enfants l'école", "Pour être riche, il faut travailler", "L'argent ne fait pas le bonheur"];
+    const tableauMessages = ["Bonjour", "Bienvenue", "Karibu", "Hello", "Vous y êtes presque"];
 }
 
 // Mets le tableau des messages ici
-const tableauMessages = ["Bonjour", "Bienvenue", "Karibu", "Hello", "Vous y êtes presque"];
 
 // Ajoutez la condition ici
 
