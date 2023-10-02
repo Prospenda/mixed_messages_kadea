fc4a1434326987b00974679efd62f287d1460edd
Ger-Cub <141303849+Ger-Cub@users.noreply.github.com>
2023-10-02 10:41:26 +0200

    Update README.mk
    Modification de readme

--- README.mk
+++ README.mk
@@ -1,1 +1,30 @@
-Ce projet consiste à créer un programme qui génère les messages de façon aléatoire. Pour ce faire, nous avons créer un tableau dans JavaScript qui contient les données (les messages). En suite nous avons créer une fontion qui permet de jénéer ou afficher un message aléatoir à chaque fois que l'utilisateur clique sur le bouton refresh.
+# Mixed Message
+
+Générateur automatique des messages aléatoires 
+## Projet
+
+- Ce projet consiste à créer un programme qui génère les messages de façon aléatoire. 
+- Pour ce faire, nous avons créer un tableau dans JavaScript qui contient les données (les messages). 
+- En suite nous avons créer une fontion qui permet de afficher un message aléatoir à chaque fois que l'utilisateur clique sur le bouton refresh.
+
+```javascript
+console.log(mixedMessages1)
+```
+Exécutez pour voir
+
+```bash
+node index.js
+```
+## Comment ça marche
+La méthode utiliser pour générer les messages aléatoires est 
+
+```javasript
+Math.random()
+```
+## Un message aléatoir à chaque fois que l'utilisateur clique sur le bouton refresh.
+
+## Resources
+(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random) on Mdn javascript Global Object
+(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array) and [Object storage](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object)
+
+
