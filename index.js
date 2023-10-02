b137501444e6ffe99ccaef348a136ddf92bd69d8
Prospenda <24062023+Prospenda@users.noreply.github.com>
2023-10-02 11:56:30 +0100

    ajout d'un nouveau tableau

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-    // const tabMessages2 = ["Tous les enfants l'école", "Pour être riche, il faut travailler", "L'argent ne fait pas le bonheur"];
+    const tabMessages2 = ["Tous les enfants l'école", "Pour être riche, il faut travailler", "L'argent ne fait pas le bonheur"];
     const tableauMessages = ["Bonjour", "Bienvenue", "Karibu", "Hello", "Vous y êtes presque"];
     
 const result = Math.floor(Math.random() * tableauMessages.length);
